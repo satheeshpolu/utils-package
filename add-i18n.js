394276b83289a054487da75f660951d37e0e5f00
satheeshpolu <satheeshpoluab@gmail.com>
2024-01-07 23:19:58 +0100

    fix: imports are working properly

--- add-i18n.js
+++ add-i18n.js
@@ -1,9 +1,48 @@
 const fs = require("fs").promises; // Use fs.promises for asynchronous file operations
 const { execSync } = require("child_process");
-const i18nUtils = require("./src/i18n-support/i18n-util.js");
 
 // Step-1: Get the project root directory
 const projectDirectoryPath = process.cwd();
+
+const fileContent = `
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import { NativeModules, Platform } from 'react-native';
+import de from './de';
+import en from './en';
+import fr from './fr';
+
+const supportedLngs = ['de', 'en', 'fr'];
+const resources = {
+  de: de,
+  en: en,
+  fr: fr,
+};
+
+const getLanguage = () => {
+  const locale =
+    Platform.OS === 'ios'
+      ? NativeModules.SettingsManager.settings.AppleLocale
+      : NativeModules.I18nManager.localeIdentifier;
+  return locale.substring(0, 2);
+};
+
+i18n.use(initReactI18next).init({
+  debug: true,
+  fallbackLng: 'de',
+  lng: getLanguage(),
+  interpolation: {
+    escapeValue: false,
+  },
+  supportedLngs: supportedLngs,
+  compatibilityJSON: 'v3',
+  resources: resources,
+});
+
+export default i18n;
+`;
+
+const i18nSupportedCountries = ["de", "fr", "en"];
 
 // Directory for translation files
 const translationsDirectory = `${projectDirectoryPath}/src/utilities/translations`;
@@ -33,7 +72,7 @@ const addDefaultTranslationsByCountry = (countryCode) => {
 // Function to create directories for supported countries
 const createDirForSupportedCountries = async () => {
 
-  for (const countryCode of i18nUtils.i18nSupportedCountries) {
+  for (const countryCode of i18nSupportedCountries) {
     const path = `${translationsDirectory}/${countryCode}`;
     //console.log(`⏳ A new ${path}/index.ts file is created.`);
     try {
@@ -77,7 +116,7 @@ const createDirForSupportedCountries = async () => {
     }
 
     // Write i18n.ts file
-    await fs.writeFile(`${translationsDirectory}/i18n.ts`, i18nUtils.fileContent);
+    await fs.writeFile(`${translationsDirectory}/i18n.ts`, fileContent);
     // console.log('i18n script added into i18n.ts file.');
     console.log("\n✅ i18n setup is done and i18n.ts file created successfully. 🚀");
   } catch (error) {
