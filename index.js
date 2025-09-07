4f91ab8b2224d8c8fb4e56ffa650440a27e1b646
satheeshpolu <satheeshpoluab@gmail.com>
2025-09-07 11:28:26 +0200

    New patch version

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const formaters = require("./src/formaters.js");
 const sortOperations = require("./src/sort-operations.js");
+const timeDifference = require("./src/time-difference.js");
 
 module.exports = {
   formatNumber: formaters.formatNumber,
   formatDate: formaters.formatDate,
   sortArray: sortOperations.sortData,
   sortArrayOfObjectsByKey: sortOperations.sortArrayObjectData,
+  calculateTimeDifference: timeDifference.calculateTimeDifference,
 };
